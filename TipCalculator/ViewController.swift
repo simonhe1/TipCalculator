71b2695fee3ac0e1472a7528a42a50656a5754a4
Simon He <simonhe@Simons-MBP.fios-router.home>
2018-08-14 18:31:57 -0400

    Added .gitignore .gitattributes

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipTotal: UILabel!
+    @IBOutlet weak var twoPeopleTip: UILabel!
+    @IBOutlet weak var threePeopleTip: UILabel!
+    @IBOutlet weak var fourPeopleTip: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func endAction(_ sender: Any) {
+        view.endEditing(true);
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let percentages = [0.15, 0.18, 0.22];
+        let bill = Double(billAmount.text!) ?? 0;
+        let tip = bill * percentages[tipControl.selectedSegmentIndex];
+        let total = bill + tip;
+        twoPeopleTip.text = String(format: "$%.2f", total/2);
+        threePeopleTip.text = String(format: "$%.2f", total/3);
+        fourPeopleTip.text = String(format: "$%.2f", total/4);
+        tipTotal.text = String(format: "$%.2f", total);
+    }
+    
 }
 
